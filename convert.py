2dc3c75912bbc13ea9256edb005ef882f3fcb76b
Daniel Libatique <daniel.libatique@gmail.com>
2018-04-18 16:53:34 -0400

    removed beta2unicode, using cltk now

--- convert.py
+++ convert.py
@@ -1,14 +1,15 @@
-import beta2unicode
 from cltk.corpus.greek.beta_to_unicode import Replacer
 import re
 
 test = r"""A)/NDRA MOI E)/NNEPE"""
 
-#r = Replacer()
+r = Replacer()
 
-#print(r.beta_code(test))
+print(r.beta_code(test))
 
+'''
 for line in open('homer_odyssey/hom_od_01.txt'):
     test = r'r\"\"\"' + line + r'\"\"\"'
     r = Replacer()
     print(r.beta_code(test))
+'''
