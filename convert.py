142390fed2e5d4127d997cc9762af408754dfdd3
Daniel Libatique <daniel.libatique@gmail.com>
2018-04-18 22:29:14 -0400

    fixed special characters, backslashes after sigmas

--- convert.py
+++ convert.py
@@ -1,15 +1,45 @@
 from cltk.corpus.greek.beta_to_unicode import Replacer
 import re
-
-test = r"""A)/NDRA MOI E)/NNEPE"""
+from os import listdir
 
 r = Replacer()
 
-print(r.beta_code(test))
+beta_files = sorted(listdir('homer_iliad/betacode_files'))
+text_files = [f for f in sorted(listdir('homer_iliad')) if f.endswith('txt')]
+zipped = zip(beta_files, text_files)
 
-'''
-for line in open('homer_odyssey/hom_od_01.txt'):
-    test = r""" + line + """
-    r = Replacer()
-    print(r.beta_code(test))
-'''
+def clean_punc(text):
+
+    punc = [r'&ldquo;', r'&rdquo;', r'&mdash;']
+    uni = ['"', '"', '-']
+    combo = zip(punc,uni)
+
+    for pair in combo:
+        text = text.replace(pair[0],pair[1])
+
+    return text
+
+def convert_to_unicode(beta,text):
+
+    #get text from betacode file
+    infile = open(beta)
+    raw = infile.read()
+    infile.close()
+
+    #remove &ldquo; &rdquo; and &mdash;
+    raw = clean_punc(raw)
+    raw = raw.split('\n')
+
+    #convert text from beta to uni and put into list of lines
+    full_text = [r.beta_code(line.upper().rstrip()) for line in raw]
+
+    #open new text file, write converted line, close when finished
+    file = open(text, 'w')
+    for line in full_text:
+        file.write(line + "\n")
+    file.close()
+
+    return full_text
+
+for pair in zipped:
+    convert_to_unicode('homer_iliad/betacode_files/' + pair[0], 'homer_iliad/' + pair[1])
