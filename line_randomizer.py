3f278edb13097d32d0b7c7f976d6f2d5a2973ff2
Daniel Libatique <daniel.libatique@gmail.com>
2018-06-13 17:01:52 -0400

    added cltk tokenizer

--- line_randomizer.py
+++ line_randomizer.py
@@ -1,29 +1,34 @@
 import random
 from os import listdir
+from cltk.tokenize.line import LineTokenizer
 
+#initialize tokenizer
+tokenizer = LineTokenizer('latin')
+
+#create list of lines
 whole_met = []
 
+#iterate through files/books of Metamorphoses
 for file in sorted(listdir('latin/ovid_metamorphoses')):
 
+    #get text from each file
     with open('latin/ovid_metamorphoses/' + file) as f:
         raw = f.read()
 
-    whole_met.extend(raw.split('\n'))
-
-clean_whole_met = list(filter(lambda a: a not '', whole_met))
+    #add line-tokenized text to the master list of lines
+    whole_met += tokenizer.tokenize(raw)
 
 #test if there are any empty lines
 def test_for_empty(list):
 
-    if '' in list:
-
+    #check for empty strings (which evaluate to False)
+    if any in list is False:
+        #get indices of empty strings
         print([i for i, x in enumerate(list) if x == ''])
 
     else:
-
         return 'No empty lines! Hooray!'
 
-print(whole_met)
-
-print(not whole_met[11996])
-#print(random.choice(whole_met))
+print(test_for_empty(whole_met))
+#print(whole_met)
+print(random.choice(whole_met))
